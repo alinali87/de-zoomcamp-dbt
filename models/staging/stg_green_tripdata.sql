3618710bc71babb94e88ab96cdcbfbe22590ae48
Alina Li <alinasdez@gmail.com>
2023-02-18 07:25:11 +0000

    partition with order by pulocationid and *dropoff_datetime

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -4,7 +4,7 @@ with tripdata as
 (
   select *,
     row_number() over(partition by vendorid, lpep_pickup_datetime 
-                        -- order by pulocationid, lpep_pickup_datetime, lpep_dropoff_datetime
+                        order by pulocationid, lpep_dropoff_datetime
                         ) as rn
   from {{ source('staging','green_tripdata') }}
   where vendorid is not null 
